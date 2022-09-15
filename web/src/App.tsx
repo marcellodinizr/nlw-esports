f375e69272965c42ae530a651bc0cd1afec46571
marcellodinizr <marcellodinizrocha@gmail.com>
2022-09-15 11:41:48 -0300

    adding the linear gradient to background

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
+import logoImg from './assets/logo-nlw.svg';
 import './styles/main.css';
 
 function App() {
  return (
-	<div>
+	<div className='max-w-[#1344px] mx-auto flex flex-col items-center m-20'>
+		<img src={logoImg}/>
 
+		<h1 className='text-6xl text-white font-black mt-20 bg-nlw-gradient bg-clip-text'>
+			Seu <span className='text-transparent'>duo</span> está aqui.
+		</h1>
 	</div>
  )
 }
