eaf045312c05a0068f4ed19f35ab442cdb81d13f
marcellodinizr <marcellodinizrocha@gmail.com>
2022-09-16 10:51:57 -0300

    fixing the eslint/prettier configs

--- web/prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-  printWidth: 200,
-  semi: true,
-  tabWidth: 2
-};
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,7 @@ import './styles/main.css';
 function App() {
 	return (
 		<div className="max-w-[#1344px] mx-auto flex flex-col items-center m-20">
-			<img src={logoImg} />
+			<img src={logoImg} alt="" />
 
 			<h1 className="text-6xl text-white font-black mt-20 bg-nlw-gradient bg-clip-text">
 				Seu <span className="text-transparent">duo</span> está aqui.
